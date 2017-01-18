c9e9230772c0ff5fc7501e6a9a5356f16eec47f9
김휘겸 <breakstorm00@gmail.com>
2017-01-18 14:42:25 +0900

    Modify 엘리먼트 안나타나는 현상 수

--- week3-class/mfacebook.js
+++ week3-class/mfacebook.js
@@ -5,37 +5,42 @@ var currentCount = 0;
 var maxCount = 80;
 console.log(mainTextarea);
 
+//HTML속성값을 변경하는 함수가 필요
+//엘리먼트, 속성네임, 속성값
+
+
+
 mainTextarea.addEventListener("focus",function(evt){
 	var mainArea = evt.target;
 	if(mainArea.tagName !== "TEXTAREA") return false;
 	mainTextarea.setAttribute("style","background-color:gray");
 	//입력공간 옆에 있는 것을 숨기고
 	var deleteButton = mainTextarea.nextElementSibling.nextElementSibling
-
 	deleteButton.previousElementSibling.style.display = "none";
 	deleteButton.style.display = "none";
 	deleteButton.nextElementSibling.style.display = "none";
-	// mainTextarea.nextElementSibling.style.visibility = "hidden";
-	// mainTextarea.nextElementSibling.nextElementSibling.style.visibility = "hidden";
-	// mainTextarea.nextElementSibling.nextElementSibling.nextElementSibling.style.visibility = "hidden";
-	//새로 만들 기능 화면에 표시
-	mainTextarea.insertAdjacentHTML("afterend","<button>hello</button>");
-	mainTextarea.insertAdjacentHTML("afterend","<p>0 / 80byte </p>");
+	//새로 만들 능 화면에 표시
+	mainTextarea.insertAdjacentHTML("afterend","<button>취소</button>");
+	mainTextarea.insertAdjacentHTML("afterend","<a>0 / 80byte </a>");
 });
 
 mainTextarea.addEventListener("focusout",function(evt){
 	var mainArea = evt.target;
-	var deleteButton = mainTextarea.nextElementSibling.nextElementSibling;
+	
 	if(mainArea.tagName !== "TEXTAREA") return false;
 	mainTextarea.setAttribute("style","background-color:white");
-	//새로 만든 기능을 숨기고 / 삭제
+	//새로 만든 능을 / 삭제
+	mainTextarea.parentElement.removeChild(mainTextarea.nextElementSibling);
 	mainTextarea.parentElement.removeChild(mainTextarea.nextElementSibling);
 	//입력한 글자값 삭제
 	mainTextarea.value = "";
-	//기존에 있던 버튼을 화면에 표
-	deleteButton.previousElementSibling.style.display = "block";
-	deleteButton.style.display = "block";
-	deleteButton.nextElementSibling.style.display = "block";
+	//기존에 있던 버튼을 화면에 표시
+	var deleteButton = mainTextarea.nextElementSibling.nextElementSibling;
+	console.log(deleteButton);
+	//deleteButton.previousElementSibling.setAttribute("style","display:block");
+	deleteButton.previousElementSibling.style.display = "";
+	deleteButton.style.display = "";
+	deleteButton.nextElementSibling.style.display = "";
 });
 
 mainTextarea.addEventListener("keyup",function(evt){
