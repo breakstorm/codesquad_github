fe6f2238c534fa14863006a69c7099ab2ad87fd3
김휘겸 <breakstorm00@gmail.com>
2017-01-19 17:17:13 +0900

    Add 17.01.18(수) 수업자료 추

--- week3-class/ajax/js/main.js
+++ week3-class/ajax/js/main.js
@@ -1,19 +1,32 @@
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load",function(evt){
-	oReq.open("GET", "js/data.json");
-	oReq.send();
-	var parseData = JSON.parse(infoSite);
-	console.log(parseData);
+	var tempObj = evt.target.responseText;
+	// console.log(evt.target);
+	var parseData = JSON.parse(tempObj);
+	// console.log(parseData);
+	var tmepHTML = "";
+	var tempSelect = document.querySelector("dl");
+	for(i in parseData.owner){
+		tmepHTML = '<dt>'+i+'</dt>' + '<dd>'+parseData.owner[i]+'</dd>';
+		tempSelect.insertAdjacentHTML("beforeend",tmepHTML);
+	}
 });
+
+
 
 var eleButton = document.querySelector("button");
 eleButton.addEventListener("click",function(evt){
 	var btn = evt.target;
 	if(btn.tagName !== "BUTTON") return false;
-	console.log("hello world");
-	var infoSite = new XMLHttpRequest();
-	infoSite.open("GET", "js/data.json");
-	infoSite.send();
-	var parseData = JSON.parse(infoSite);
-	console.log(parseData);
+	// var btnParent = btn.parentElement;
+	// console.log("hello world");
+	btn.setAttribute("style","display:none");
+	oReq.open("GET", "js/data.json");
+	oReq.send();
+	// var tmepHTML = "";
+	// var tempSelect = document.querySelector("dl");
+	// for(i in parseData.owner){
+	// 	tmepHTML = '<dt>'+i+'</dt>' + '<dd>'+parseData.owner[i]+'</dd>';
+	// 	tempSelect.insertAdjacentHTML("beforeend",tmepHTML);
+	// }
 });
