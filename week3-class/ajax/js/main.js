c1489ac92e556ae584cd3a4a653d93d6166b0062
김휘겸 <breakstorm00@gmail.com>
2017-01-19 17:46:46 +0900

    Modify 코드리뷰 후 수정사항 반

--- week3-class/ajax/js/main.js
+++ week3-class/ajax/js/main.js
@@ -1,32 +1,26 @@
 var oReq = new XMLHttpRequest();
+var eleButton = document.querySelector("button");
+
+
 oReq.addEventListener("load",function(evt){
 	var tempObj = evt.target.responseText;
-	// console.log(evt.target);
 	var parseData = JSON.parse(tempObj);
-	// console.log(parseData);
-	var tmepHTML = "";
+	var tmepHTML = ""; //오타
 	var tempSelect = document.querySelector("dl");
-	for(i in parseData.owner){
+	//var선언 필요.
+	for(var i in parseData.owner){
+		//마크업이 수정될시 이슈사항 발생.
 		tmepHTML = '<dt>'+i+'</dt>' + '<dd>'+parseData.owner[i]+'</dd>';
 		tempSelect.insertAdjacentHTML("beforeend",tmepHTML);
 	}
 });
 
+//추가 과제 oReq.status값 조건분기 작성
 
-
-var eleButton = document.querySelector("button");
 eleButton.addEventListener("click",function(evt){
 	var btn = evt.target;
 	if(btn.tagName !== "BUTTON") return false;
-	// var btnParent = btn.parentElement;
-	// console.log("hello world");
 	btn.setAttribute("style","display:none");
 	oReq.open("GET", "js/data.json");
 	oReq.send();
-	// var tmepHTML = "";
-	// var tempSelect = document.querySelector("dl");
-	// for(i in parseData.owner){
-	// 	tmepHTML = '<dt>'+i+'</dt>' + '<dd>'+parseData.owner[i]+'</dd>';
-	// 	tempSelect.insertAdjacentHTML("beforeend",tmepHTML);
-	// }
 });
