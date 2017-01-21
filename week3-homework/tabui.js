98bdbfa66452864c803f43e1e9f8e94636844c24
김휘겸 <breakstorm00@gmail.com>
2017-01-22 00:34:36 +0900

    Modify 프로토타입 완성(HTML, CSS, JS)

--- week3-homework/tabui.js
+++ week3-homework/tabui.js
@@ -3,9 +3,14 @@ var eleFriend = document.querySelector("#friend");
 var eleTheme = document.querySelector("#theme");
 var eleNews = document.querySelector("#news");
 
-var oReq = new XMLHttpRequest()
+var oReqPosition = new XMLHttpRequest()
+var oReqFriend = new XMLHttpRequest()
+var oReqTheme = new XMLHttpRequest()
+var oReqNews = new XMLHttpRequest()
 
-oReq.addEventListener("load",function(evt){
+//함수를 리팩토링 하는 방법은 없을까?
+
+oReqPosition.addEventListener("load",function(evt){
 	var tempElement = document.querySelector(".eleDisplayShow > ul > li")
 	var tempXHRObj = evt.target.responseText;
 	var tempXHRText = JSON.parse(tempXHRObj);
@@ -15,12 +20,73 @@ oReq.addEventListener("load",function(evt){
 	// 	tempInsertString = '<div>'
 	// 	console.log(i + tempResponseText[i]);
 	// }
-})
+});
+
 
 elePostion.addEventListener("click",function(evt){
 	var tempButton = evt.target;
 	//if(tempButton.tagName !== "BUTTON") return false;
-	oReq.open("GET", "http://jsonplaceholder.typicode.com/posts/1");
-	oReq.send();
+	oReqPosition.open("GET", "http://jsonplaceholder.typicode.com/posts/1");
+	oReqPosition.send();
 });
 
+
+oReqFriend.addEventListener("load",function(evt){
+	var tempElement = document.querySelector("#my_friend > ul > li")
+	var tempXHRObj = evt.target.responseText;
+	var tempXHRText = JSON.parse(tempXHRObj);
+	var tempInsertString = '<div>' + tempXHRText["title"] + '</div>' + '<div>' + tempXHRText["body"] + '</div>';
+	tempElement.insertAdjacentHTML("beforeend",tempInsertString);
+	// for(i in tempResponseText){
+	// 	tempInsertString = '<div>'
+	// 	console.log(i + tempResponseText[i]);
+	// }
+});
+
+
+eleFriend.addEventListener("click",function(evt){
+	var tempButton = evt.target;
+	//if(tempButton.tagName !== "BUTTON") return false;
+	oReqFriend.open("GET", "http://jsonplaceholder.typicode.com/posts/2");
+	oReqFriend.send();
+});
+
+
+oReqTheme.addEventListener("load",function(evt){
+	var tempElement = document.querySelector("#my_theme > ul > li")
+	var tempXHRObj = evt.target.responseText;
+	var tempXHRText = JSON.parse(tempXHRObj);
+	var tempInsertString = '<div>' + tempXHRText["title"] + '</div>' + '<div>' + tempXHRText["body"] + '</div>';
+	tempElement.insertAdjacentHTML("beforeend",tempInsertString);
+	// for(i in tempResponseText){
+	// 	tempInsertString = '<div>'
+	// 	console.log(i + tempResponseText[i]);
+	// }
+});
+
+
+eleTheme.addEventListener("click",function(evt){
+	var tempButton = evt.target;
+	//if(tempButton.tagName !== "BUTTON") return false;
+	oReqTheme.open("GET", "http://jsonplaceholder.typicode.com/posts/3");
+	oReqTheme.send();
+});
+
+oReqNews.addEventListener("load",function(evt){
+	var tempElement = document.querySelector("#my_news > ul > li")
+	var tempXHRObj = evt.target.responseText;
+	var tempXHRText = JSON.parse(tempXHRObj);
+	var tempInsertString = '<div>' + tempXHRText["title"] + '</div>' + '<div>' + tempXHRText["body"] + '</div>';
+	tempElement.insertAdjacentHTML("beforeend",tempInsertString);
+	// for(i in tempResponseText){
+	// 	tempInsertString = '<div>'
+	// 	console.log(i + tempResponseText[i]);
+	// }
+});
+
+eleNews.addEventListener("click",function(evt){
+	var tempButton = evt.target;
+	//if(tempButton.tagName !== "BUTTON") return false;
+	oReqNews.open("GET", "http://jsonplaceholder.typicode.com/posts/4");
+	oReqNews.send();
+});
