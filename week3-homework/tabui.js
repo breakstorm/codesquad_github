f25273567d75b0e40beba65538d2a7bd2324ffab
김휘겸 <breakstorm00@gmail.com>
2017-01-22 20:04:48 +0900

    Modify 예외 이벤트 처

--- week3-homework/tabui.js
+++ week3-homework/tabui.js
@@ -10,6 +10,39 @@ var oReqNews = new XMLHttpRequest()
 
 //함수를 리팩토링 하는 방법은 없을까?
 
+function removeTabContent(){
+	var tempElement1 = document.querySelector(".navPosition");
+	var tempElement2 = document.querySelector(".navFriend");
+	var tempElement3 = document.querySelector(".navTheme");
+	var tempElement4 = document.querySelector(".navNews");
+	if(tempElement1.firstElementChild){
+		for(i=0; i<2; i++){
+		tempElement1.removeChild(tempElement1.firstElementChild);
+		}	
+	}
+	if(tempElement2.firstElementChild){
+		for(i=0; i<2; i++){
+		tempElement2.removeChild(tempElement2.firstElementChild);
+		}	
+	}
+	if(tempElement3.firstElementChild){
+		for(i=0; i<2; i++){
+		tempElement3.removeChild(tempElement3.firstElementChild);
+		}	
+	}
+	if(tempElement4.firstElementChild){
+		for(i=0; i<2; i++){
+		tempElement4.removeChild(tempElement4.firstElementChild);
+		}	
+	}
+	// for(i=0; i<2; i++){
+	// 	tempElement1.removeChild(tempElement1.firstElementChild);
+	// 	tempElement2.removeChild(tempElement2.firstElementChild);
+	// 	tempElement3.removeChild(tempElement3.firstElementChild);
+	// 	tempElement4.removeChild(tempElement4.firstElementChild);
+	// }
+}
+
 oReqPosition.addEventListener("load",function(evt){
 	var tempElement = document.querySelector(".eleDisplayShow > ul > li")
 	if(tempElement.innerText) return false;
@@ -27,6 +60,7 @@ oReqPosition.addEventListener("load",function(evt){
 elePostion.addEventListener("click",function(evt){
 	var tempButton = evt.target;
 	//if(tempButton.tagName !== "BUTTON") return false;
+	removeTabContent();
 	oReqPosition.open("GET", "http://jsonplaceholder.typicode.com/posts/1");
 	oReqPosition.send();
 });
@@ -49,6 +83,7 @@ oReqFriend.addEventListener("load",function(evt){
 eleFriend.addEventListener("click",function(evt){
 	var tempButton = evt.target;
 	//if(tempButton.tagName !== "BUTTON") return false;
+	removeTabContent();
 	oReqFriend.open("GET", "http://jsonplaceholder.typicode.com/posts/2");
 	oReqFriend.send();
 });
@@ -71,6 +106,7 @@ oReqTheme.addEventListener("load",function(evt){
 eleTheme.addEventListener("click",function(evt){
 	var tempButton = evt.target;
 	//if(tempButton.tagName !== "BUTTON") return false;
+	removeTabContent();
 	oReqTheme.open("GET", "http://jsonplaceholder.typicode.com/posts/3");
 	oReqTheme.send();
 });
@@ -91,6 +127,7 @@ oReqNews.addEventListener("load",function(evt){
 eleNews.addEventListener("click",function(evt){
 	var tempButton = evt.target;
 	//if(tempButton.tagName !== "BUTTON") return false;
+	removeTabContent();
 	oReqNews.open("GET", "http://jsonplaceholder.typicode.com/posts/4");
 	oReqNews.send();
 });
